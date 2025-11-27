89afd00b6e7618b624b5363354f6daf66c91d3af
yousef-ramadan1907 <yo543113@gmail.com>
2025-11-27 17:20:47 +0200

    new edit for menu page and navbar

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,37 @@
 let pages = document.getElementsByClassName("pages");
-let drink_1 = document.getElementById("drink-1");
+let title_page = document.querySelector(".title-page");
+let title = document.querySelector(".title");
+ const menuItems = document.querySelectorAll('.dropdown-item');
+const navbarCollapse = document.getElementById('navbarSupportedContent');
+
 function remove() {
     for (let i = 0; i < pages.length; i++) {
-        pages[i].className = 'd-none';
+        pages[i].classList.add('d-none');
     }
 }
+
 function Switch_pages(PageId) {
     remove();
     let page = document.getElementById(PageId);
-    page.className = "container mt-4 pages";
+    page.classList.remove("d-none");
+    title.classList.remove("d-none");
+
+
+    title_page.scrollIntoView({
+        behavior: "smooth"
+    });
 }
 
-
-export { Switch_pages };
+    menuItems.forEach(item => {
+        item.addEventListener('click', () => {
+            // اقفل الـ navbar لو مفتوح
+            if (navbarCollapse.classList.contains('show')) {
+                const bsCollapse = new bootstrap.Collapse(navbarCollapse);
+                bsCollapse.hide();
+                
+            }
+             title_page.scrollIntoView({
+        behavior: "smooth"
+    });
+        });
+    });
